63c2cab8c9de25916fe875b165272c16fdfb4e90
Charles <charlesjiangxm@gmail.com>
2026-06-14 10:58:02 +0800

    change the position of cmodel

--- hw/verif/flist/multihead_attention_filelist.f
+++ hw/verif/flist/multihead_attention_filelist.f
@@ -1,6 +1,6 @@
 // HDL sources for the MultiheadAttention end-to-end DPI-C check.
 // The DPI glue (utils/multihead_attention_dpi.c) and the C model
-// (src/models/multihead_attention_cmodel.c) are passed directly on the vcs
+// (hw/model/multihead_attention_cmodel.c) are passed directly on the vcs
 // command line by the Makefile.
 ../rtl/multihead_attention.v
 tb/tb_multihead_attention.sv
